153ebdab8999259901105ef33c255d35e57c9ff9
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2021-01-26 18:11:59 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -11,7 +11,7 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={(e)=>this.state={e.target.value}} />
       </div>
     );
   }
