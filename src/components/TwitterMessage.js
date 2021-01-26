f8b5313b5d58e9867940726c11ef2d76ac1ec163
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2021-01-26 18:14:55 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,16 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: " "
+    };
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.} />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.value} />
       </div>
     );
   }
